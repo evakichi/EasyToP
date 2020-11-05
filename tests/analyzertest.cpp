1eb989ca9e572457cbf0b0a51761982544b4a4ae
Makoto ISHIHARA <evakichi@be.wakwak.com>
2020-11-05 22:14:50 +0900

    create test code.

---
+++ tests/analyzertest.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <clang-c/Index.h>
+
+using namespace std;
+
+main(int argc,char *argv[])
+{
+	CXIndex Idx = clang_createIndex(1,1);
+	CXTranslationUnit TU = clang_createTranslationUnit(Idx, argv[1]);
+	CXCursor cursor = clang_getTranslationUnitCursor(TU);
+
+	CXType type = clang_getCursorType(cursor);
+	CXString typeSpelling = clang_getTypeSpelling(type);
+	cout << clang_getCString(typeSpelling) << endl;
+
+	clang_disposeTranslationUnit(TU);
+	clang_disposeIndex(Idx);
+}
+
